970103970582990577987a6232b16bc2398c2a8d
Jimmy <60604723+A85121313@users.noreply.github.com>
2020-02-05 12:47:17 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,8 +1,8 @@
  navigator.geolocation.getCurrentPosition(function(position){
 	var lat = position.coords.latitude ;
 	var lng = position.coords.longitude;	
-    new google.maps.Map(document.getElementById("map_1"),{
-    zoom: 20,
-    center: new google.maps.LatLng( lat , lng )
-	});
+	new google.maps.Map(document.getElementById("map_1"),{
+   		zoom: 20,
+   		center: new google.maps.LatLng( lat , lng )
+		});
 	});
