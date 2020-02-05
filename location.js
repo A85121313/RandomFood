8c2e4a61f7b99de400fd74061c4c4ae984350f5b
Jimmy <60604723+jimmy12487@users.noreply.github.com>
2020-02-05 20:12:17 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,7 +1,7 @@
  navigator.geolocation.getCurrentPosition(function(position){
 	var lat = position.coords.latitude ;
 	var lng = position.coords.longitude;	
-	new google.maps.Map(document.getElementById("map"),{
+	new google.maps.Map(document.getElementById("currentmap"),{
    		zoom: 20,
    		center: new google.maps.LatLng( lat , lng )
 		});
