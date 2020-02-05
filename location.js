397f2e7dfa44b48329bf9d3fd391a009bd39a233
Jimmy <60604723+A85121313@users.noreply.github.com>
2020-02-05 12:41:28 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,4 +1,8 @@
-navigator.geolocation.getCurrentPosition(function(position){
-	var lon = position.coords.longitude;
-	var lat = position.coords.latitude;
-});
+ navigator.geolocation.getCurrentPosition(function(position){
+	var lat = position.coords.latitude ;
+	var lng = position.coords.longitude;	
+    new google.maps.Map(document.getElementById("map_1"), { // (JavaScript寫法)
+    zoom: 20,
+    center: new google.maps.LatLng( lat , lng ) // LatLng()建立經緯度的物件
+	});
+	});
