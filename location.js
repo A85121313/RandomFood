1cdaab2806869bd7c04c30269180bfe130b4765a
Jimmy <60604723+jimmy12487@users.noreply.github.com>
2020-02-12 16:49:19 +0800

    Delete location.js

--- location.js
+++
@@ -1,8 +0,0 @@
- navigator.geolocation.getCurrentPosition(function(position){
-	var lat = position.coords.latitude ;
-	var lng = position.coords.longitude;	
-	new google.maps.Map(document.getElementById("currentmap"),{
-   		zoom: 20,
-   		center: new google.maps.LatLng( lat , lng )
-		});
-	});
